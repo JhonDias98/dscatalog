1ca7a1a4abad3f859e7ad9409287688fb80f8df9
Jonathan Dias <jonathan.dias.p2017@gmail.com>
2021-10-08 00:50:40 -0300

    H2 security configuration

--- backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServiceConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServiceConfig.java
@@ -2,6 +2,7 @@ package com.devsuperior.dscatalog.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -9,14 +10,19 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+import java.util.Arrays;
+
 @Configuration
 @EnableResourceServer
 public class ResourceServiceConfig extends ResourceServerConfigurerAdapter {
 
     @Autowired
+    private Environment environment;
+
+    @Autowired
     private JwtTokenStore tokenStore;
 
-    private static final String[] PUBLIC = {"/oauth/token"};
+    private static final String[] PUBLIC = {"/oauth/token", "/h2-console/**"};
     private static final String[] OPERATOR_OR_ADMIN = {"/products/**", "/categories/**"};
     private static final String[] ADMIN = {"/users/**"};
     @Override
@@ -26,6 +32,12 @@ public class ResourceServiceConfig extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
+
+        //Liberar H2
+        if(Arrays.asList(environment.getActiveProfiles()).contains("test")) {
+            http.headers().frameOptions().disable();
+        }
+
         http.authorizeRequests()
         .antMatchers(PUBLIC).permitAll()
         .antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll()
